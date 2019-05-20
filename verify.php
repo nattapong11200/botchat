82a7d64f49614664848656c9f8ebd5c9c4224751
nattapong11200 <50147369+nattapong11200@users.noreply.github.com>
2019-05-20 12:19:28 +0700

    Update verify.php

--- verify.php
+++ verify.php
@@ -1,5 +1,5 @@
 <?php
-$access_token = 'XXXXXXXXXXXXXXXXXXXXXXX';
+$access_token = '187XNu4NuZLewXkyjiMfLhHhjum7W89wSxQdTQUJ7eH5qnhOTNqdJko8Of+hvXXUQD3cjQNp1PDfob1joHuZVq79sdcOrHd8sPHTaklk+AQmqRk/k/i9hky61BG25XTb9LCQq2SlZPzET1YUpWgC7AdB04t89/1O/w1cDnyilFU=';
 $url = 'https://api.line.me/v1/oauth/verify';
 $headers = array('Authorization: Bearer ' . $access_token);
 $ch = curl_init($url);curl_setopt($ch, CURLOPT_RETURNTRANSFER, true);
